f15c3860487f8fa913eefeb737cb637aaa83624a
shinyo627 <shinyo627@gmail.com>
2021-06-10 13:58:17 +0200

    UserSearch component has been updated with search functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import ReactDom from 'react-dom';
-import GuestList from './state/GuestList';
+// import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
 
 const App = () => {
   return (
     <div>
       <h1>Hi there!</h1>
-      <GuestList />
+      {/* <GuestList /> */}
+      <UserSearch />
     </div>
   );
 };
--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,7 +1,42 @@
 import { useState } from 'react';
 
+const users = [
+  { name: 'Gregg', age: 23 },
+  { name: 'Yoseob', age: 30 },
+  { name: 'Timothy', age: 33 },
+];
+
 const UserSearch: React.FC = () => {
-  return <div>user search</div>;
+  const [name, setName] = useState('');
+  const [searched, setSearched] =
+    useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name[0].toUpperCase() + name.slice(1);
+    });
+    setSearched(foundUser);
+
+    // users.filter((user) => {
+    //   return user.name === name;
+    // });
+  };
+
+  return (
+    <div>
+      User Search
+      <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find user</button>
+      <div>
+        {searched && searched.name}
+        {searched && searched.age}
+      </div>
+    </div>
+  );
 };
 
 export default UserSearch;
